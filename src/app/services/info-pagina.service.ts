b95f90a8cb87259a2c83be8d47e9c09a241d86e5
Victor Alvarez <vitoalvareza@gmail.com>
2018-08-20 17:15:17 -0300

    fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,11 +9,19 @@ export class InfoPaginaService {
 
   info: InfoPagina  = {};
   cargada = false;
+  equipo: any[] = [];
 
 
   constructor(private http: HttpClient) {
 
-    //console.log("Servicio de infoPagina Listo");
+    this.cargarInfo();
+    this.cargarEquipo();
+
+   
+  }
+
+  private cargarInfo (){
+
     //Leer el archivo JSON
 
     this.http.get('assets/data/data-pagina.json')
@@ -21,13 +29,24 @@ export class InfoPaginaService {
 
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
 
     });
 
+  }
 
+  private cargarEquipo(){
 
+     //Leer el archivo JSON
 
+     this.http.get('https://portafolioweb-47021.firebaseio.com/equipo.json')
+     .subscribe(( resp: any []) => {
+ 
+    
+       this.equipo = resp;
+       //console.log(resp);
+ 
+     });
 
-   }
+  }
+
 }
